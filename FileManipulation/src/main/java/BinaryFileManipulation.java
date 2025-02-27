cfc5fb86aad3fc19d906b8cfe23b884d8c1a5210
vitor-silva-franca <sf_vitor@outlook.com>
2025-02-27 11:58:11 -0500

    features for BinaryFileManipulation

--- FileManipulation/src/main/java/BinaryFileManipulation.java
+++ FileManipulation/src/main/java/BinaryFileManipulation.java
@@ -1,8 +1,35 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class BinaryFileManipulation {
 
     public static void main(String[] args) {
+        String filename = "example.bin";
+        String data = "Hello, World!";
 
+        //writeBinaryFile(filename, data);
+        //readBinaryFile(filename);
     }
 
+    public static void writeBinaryFile(String filename, String data) {
+        try {
+            FileOutputStream fos = new FileOutputStream(filename);
+            fos.write(data.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void readBinaryFile(String filename) {
+        try (FileInputStream fis = new FileInputStream(filename)) {
+            int byteData;
+            while ((byteData = fis.read()) != -1) {
+                System.out.print((char) byteData);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
